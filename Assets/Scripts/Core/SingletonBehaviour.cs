0f3504b5688eaf1a6be1b914382ed614247362f4
Noah Ratcliff <me@nratcliff.com>
2018-04-08 10:48:02 -0400

    setup some base stuff

---
+++ Assets/Scripts/Core/SingletonBehaviour.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using UnityEngine;
+
+/// <summary>
+/// Parent class for singleton MonoBehaviours. 
+/// </summary>
+/// <typeparam name="T">The type of the child class</typeparam>
+public abstract class SingletonBehaviour<T> : MonoBehaviour where T : SingletonBehaviour<T>
+{
+    protected static T instance;
+    public static T Instance
+    {
+        get
+        {
+            if (instance != null)
+                return instance;
+            else
+            {
+                Debug.LogWarningFormat(
+                    "[SingletonBehaviour<{0}>] Attempted to get Instance of singleton, but the instance is null!",
+                    typeof(T).ToString());
+                return null;
+            }
+        }
+    }
+
+    protected virtual void Awake()
+    {
+        if (instance == null)
+        {
+            setInstance();
+        }
+        else if(instance != this)
+        {
+            replaceInstance();
+        }
+    }
+
+    /// <summary>
+    /// Sets the current instance of the singleton. Be sure to set `instance` before returning!
+    /// </summary>
+    protected abstract void setInstance();
+
+    /// <summary>
+    /// Handles behaviour of singleton when there's already a singleton instance
+    /// </summary>
+    protected virtual void replaceInstance()
+    {
+        Debug.LogWarningFormat(
+            "[SingletonBehaviour<{0}>] An instance of the singleton already exists in the scene, destroying self. Override replaceInstance() in child class to change behaviour.",
+            typeof(T).ToString());
+
+        DestroyImmediate(this);
+    }
+}
---
+++ Assets/Scripts/InputManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Singleton, gets gyroscope info from the phone (or PS4 controller in debug mode),
+/// and has a delegate for gyro input update 
+/// </summary>
+public class InputManager : SingletonBehaviour<InputManager>
+{
+    protected override void setInstance()
+    {
+        instance = this;
+    }
+
+    protected override void Awake()
+    {
+        base.Awake(); // set or replace instance
+
+
+    }
+}
