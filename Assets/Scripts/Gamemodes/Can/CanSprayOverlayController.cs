b2caddaeef01ce52f2ebbc4ae5ce98d199c0a90c
Noah Ratcliff <me@nratcliff.com>
2018-04-16 21:50:58 -0400

    add can spray animation

---
+++ Assets/Scripts/Gamemodes/Can/CanSprayOverlayController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CanSprayOverlayController : MonoBehaviour
+{
+    private void Start()
+    {
+        CanController.Instance.OnCanBrokenEvent += activateSelf;
+        gameObject.SetActive(false); // deactivate self
+    }
+
+    private void activateSelf()
+    {
+        gameObject.SetActive(true);
+        CanController.Instance.OnCanBrokenEvent -= activateSelf;
+    }
+}
