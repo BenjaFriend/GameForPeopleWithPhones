34b98c3009b0041d36dee37bfae2fae879e93257
rwaring <rowanwaring@gmail.com>
2018-04-16 23:40:16 -0400

    added basic audio loop for can game - need to implement the win oneshot song somehow

---
+++ Assets/Scripts/Core/Audio/MusicManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicManager : MonoBehaviour
+{
+    public AudioClip Song;
+
+    private PooledAudioSource _source;
+
+    // Use this for initialization
+    void Start()
+    {
+        if (_source == null)
+            _source = AudioManager.Instance.Play(Song, AudioPoolType.Music, Constants.Mixer.Mixers.Master.Music.Name, true, () => { _source = null; });
+    }
+
+    private void OnDestroy()
+    {
+        if (_source != null)
+            _source.Stop();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
